d5e024594b9c6410566ac86ba670f3c6641635ad
Ivannnkaaa <ivanmatsuk@vk.com>
2025-05-25 21:39:15 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-	std::cout << "Hello\n";
+	cout << "Laboratory work #8. GIT\n";
+    	cout << "Variant #9. Library Subscription\n";
+    	cout << "Author: Ivan Matsuk\n";
 	return 0;
 }
